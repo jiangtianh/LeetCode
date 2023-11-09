135b7ff984ccb2ecd935e4e169035f950dc0a5a8
Jiangtian Han <112032591+jiangtianh@users.noreply.github.com>
2023-11-09 14:22:22 -0800

    Time: 406 ms (66.24%), Space: 17.6 MB (32.23%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,28 +1,21 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-
-        self.res = False
-        self.cache = {}
-
-        def f(i):
-            if i == len(nums) - 1:
-                return True 
-            elif i >= len(nums):
-                return False 
-            else:
-                
-                if i in self.cache:
-                    return self.cache[i]
-                
-                steps = nums[i]
-                
-                res = False
-                for step in range(steps, 0, -1):
-                    res = f(i + step) or res
-                    if res:
-                        break
-
-                self.cache[i] = res
-                return res
         
-        return f(0)
+        
+        furthest = 0
+
+        
+        for i in range(len(nums)):
+            if i > furthest:
+                return False 
+            furthest = max(furthest, i + nums[i])
+
+        return furthest >= len(nums) - 1
+
+
+            
+            
+
+                
+
+

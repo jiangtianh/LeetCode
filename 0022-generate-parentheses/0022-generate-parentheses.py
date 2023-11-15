10893664901c51ed7e56d9d1cf7e583115d05ea5
Jiangtian Han <112032591+jiangtianh@users.noreply.github.com>
2023-11-15 11:19:25 -0800

    Time: 52 ms (8.08%), Space: 16.6 MB (62.25%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,21 +1,22 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         self.res = []
-
-        def f(l, r, temp):
-            if l < r or l > n or r > n:
+        self.temp = []
+        
+        def f(l, r):
+            if r > l or l > n or r > n:
                 return 
             if l == r == n:
-                self.res.append("".join(temp))
+                self.res.append("".join(self.temp))
                 return 
             
-            temp.append("(")
-            f(l + 1, r, temp)   
-            temp.pop()
-
-            temp.append(")")
-            f(l, r + 1, temp)
-            temp.pop()
-
-        f(0, 0, [])
+            self.temp.append("(")
+            f(l + 1, r)
+            self.temp.pop()
+            
+            self.temp.append(")")
+            f(l, r + 1)
+            self.temp.pop()
+            
+        f(0, 0)
         return self.res

b3af56ad5efba2a9a541218b99197ab4f0483f57
Jiangtian Han <112032591+jiangtianh@users.noreply.github.com>
2023-11-22 12:52:28 -0800

    Time: 1083 ms (34.39%), Space: 37.2 MB (14.55%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -2,20 +2,19 @@ class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         @cache
         def f(amount):
-            if amount == 0:
-                return 0
-            elif amount < 0:
-                return math.inf
-            
+            if amount < 0:
+                return math.inf 
+            elif amount == 0:
+                return 0 
             res = math.inf
-
             for coin in coins:
-                res = min(res, f(amount - coin))
+                temp = f(amount - coin)
+                if temp != math.inf:
+                    res = min(res, temp + 1)
             
-            return res + 1
+            return res
         
         res = f(amount)
         if res == math.inf:
             return -1
-        else:
-            return res
+        return res

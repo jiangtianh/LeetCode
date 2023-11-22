6ae8b1d605b30da7680a95ace5e10ae6dfe8ba82
Jiangtian Han <112032591+jiangtianh@users.noreply.github.com>
2023-11-22 11:12:56 -0800

    Time: 1165 ms (43.30%), Space: 173.2 MB (8.64%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.py
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.py
@@ -1,12 +1,10 @@
 class Solution:
     def maximumScore(self, nums: List[int], multipliers: List[int]) -> int:
-        
         @cache
         def f(l, r, i):
             if i == len(multipliers):
-                return 0
+                return 0 
+            
+            return max(nums[l] * multipliers[i] + f(l + 1, r, i + 1), nums[r] * multipliers[i] + f(l, r - 1, i + 1))
         
-            return max(f(l + 1, r, i + 1) + multipliers[i] * nums[l], f(l, r - 1, i + 1) + multipliers[i] * nums[r])
-
-
         return f(0, len(nums) - 1, 0)

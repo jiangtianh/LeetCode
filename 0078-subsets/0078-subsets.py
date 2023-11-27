6b5dc06e93c4172b60d1afed1105ea1ceea2d84b
Jiangtian Han <112032591+jiangtianh@users.noreply.github.com>
2023-11-26 20:08:06 -0800

    Time: 39 ms (70.82%), Space: 16.5 MB (19.19%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,15 +1,16 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        
         self.res = []
+        
         def f(i, temp):
             if i == len(nums):
                 self.res.append(temp[:])
                 return 
+            
             temp.append(nums[i])
             f(i + 1, temp)
-            temp.pop()
-            f(i + 1, temp)
-
+            temp.pop() 
+            f(i+1, temp)
+        
         f(0, [])
         return self.res

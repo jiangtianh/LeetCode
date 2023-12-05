d98942f76763e8682273a9775140ae09dc301414
Jiangtian Han <112032591+jiangtianh@users.noreply.github.com>
2023-12-04 19:35:37 -0800

    Time: 41 ms (79.13%), Space: 16.7 MB (32.55%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -1,18 +1,10 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
         citations.sort(reverse=True) 
-        
-        def getNumOfPaper(i):
-            return len(citations) - i 
-        
-        print(citations)
         res = 0
         
-        for i, citation in enumerate(citations):
-            num = i + 1
-            if citation >= num:
-                res = max(res, min(num, citation))
-            else:
-                break
+        for i in range(len(citations)):
+            if citations[i] >= i + 1:
+                res = max(res, i+1)
+                
         return res
-        

819033649a5ba7ce94e2fe67b90c4c75fa5d0ba4
Jiangtian Han <112032591+jiangtianh@users.noreply.github.com>
2023-12-05 10:35:42 -0800

    Time: 63 ms (45.76%), Space: 16.4 MB (69.40%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -1,28 +1,24 @@
-from collections import deque
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        if len(s) <= numRows or numRows == 1:
+        if numRows == 1:
             return s
-
-
-        itemPerIteration = numRows + numRows - 2
-        numOfContainers = math.ceil(len(s) / itemPerIteration)
-
-        container = []
-        for i in range(numOfContainers):
-            cur = deque(s[i * itemPerIteration:(i+1) * itemPerIteration])
-            container.append(cur)
         
-        print(container)
-        res = ""
-        for row in range(numRows):
-            for i in range(numOfContainers):
-                currentContainer = container[i]
-
-                if row in range(len(container[i])):
-                    res += container[i][row]
-                if itemPerIteration - row in range(len(currentContainer)):
-                    if row != 0 and row != numRows - 1:
-                        res += container[i][itemPerIteration - row]
-
-        return res
+        res = [""] * numRows
+        
+        direction = -1
+        pointer = 0
+        
+        for i in range(len(s)):
+            c = s[i]
+            res[pointer] += c
+            
+            if pointer == 0 or pointer == len(res) - 1:
+                direction *= -1
+            
+            pointer += direction
+            
+        
+        return "".join(res)
+        
+        
+        

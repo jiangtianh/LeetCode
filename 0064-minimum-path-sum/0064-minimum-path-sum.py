7d94f97fc4a9fc58c2b664fbc814e7e33638d19e
Jiangtian Han <112032591+jiangtianh@users.noreply.github.com>
2023-12-20 12:34:16 -0800

    Time: 92 ms (67.11%), Space: 19.2 MB (14.96%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,17 +1,26 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        @cache
-        def f(x, y):
-            if x == len(grid) - 1 and y == len(grid[0]) - 1:
-                return grid[x][y]
-            elif x == len(grid) - 1:
-                return grid[x][y] + f(x, y + 1)
-            elif y == len(grid[0]) - 1:
-                return grid[x][y] + f(x + 1, y)
-            
-            return grid[x][y] + min(f(x + 1, y), f(x, y+1))
-        return f(0,0)
-            
+        
+        res = grid.copy() 
+        
+        for x in reversed(range(len(grid))):
+            for y in reversed(range(len(grid[0]))):
+                
+                if x == len(grid) - 1 and y == len(grid[0]) - 1:
+                    res[x][y] = grid[x][y] 
+                elif x == len(grid) - 1:
+                    res[x][y] = grid[x][y] + res[x][y+1]
+                elif y == len(grid[0]) - 1:
+                    res[x][y] = grid[x][y] + res[x+1][y]
+                else:
+                    res[x][y] = grid[x][y] + min(res[x+1][y], res[x][y+1])
+        
+        return res[0][0]
+        
+        
+        
+        
+     
             
             
             

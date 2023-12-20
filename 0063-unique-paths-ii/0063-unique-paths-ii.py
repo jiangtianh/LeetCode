f5605a80fbfccd2bf8fc27853a9f946cebd70b79
Jiangtian Han <112032591+jiangtianh@users.noreply.github.com>
2023-12-20 12:20:39 -0800

    Time: 49 ms (52.77%), Space: 17.7 MB (10.52%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -1,15 +1,16 @@
 class Solution:
-    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+    def uniquePathsWithObstacles(self, grid: List[List[int]]) -> int:
         @cache
         def f(x, y):
-            if x >= len(obstacleGrid) or y >= len(obstacleGrid[0]) or obstacleGrid[x][y] == 1:
+            if x == len(grid) or y == len(grid[0]) or grid[x][y] == 1:
                 return 0 
-            if x == len(obstacleGrid) - 1 and y == len(obstacleGrid[0]) - 1:
+            elif x == len(grid) - 1 and y == len(grid[0]) - 1:
                 return 1
+            
             return f(x+1, y) + f(x, y+1)
-        
-        return f(0, 0)
+
+
             
-            
+  
         
-        
+        return f(0,0)

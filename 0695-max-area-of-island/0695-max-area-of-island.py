b2edb240f4fd02012b6daab675552ebd58e4626f
Jiangtian Han <112032591+jiangtianh@users.noreply.github.com>
2023-12-24 01:52:15 -0800

    Time: 128 ms (59.07%), Space: 18 MB (71.38%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,35 +1,21 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-        m = len(grid[0])
-        n = len(grid)
         
-        
+        directions = [[0, 1], [1, 0], [-1, 0], [0, -1]]
         def check(x, y):
-            return x in range(n) and y in range(m) and grid[x][y] == 1
-        
-        
-        def bfs(x, y):
-            q = collections.deque() 
-            q.append((x, y))
-            res = 0
-            while q:
-                curX, curY = q.popleft()
-                res += 1
-                for i, j in directions:
-                    newX, newY = curX + i, curY + j
-                    if check(newX, newY):
-                        grid[newX][newY] = 0
-                        q.append((newX, newY))
+            return x in range(len(grid)) and y in range(len(grid[0])) and grid[x][y] == 1
+    
+        def dfs(x, y):
+            res = 1
+            grid[x][y] = 0
+            for i, j in directions:
+                newX, newY = x + i, y + j
+                if check(newX, newY):
+                    res += dfs(newX, newY)
             return res
-            
         res = 0
-        for i in range(n):
-            for j in range(m):
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
                 if grid[i][j] == 1:
-                    grid[i][j] = 0
-                    res = max(res, bfs(i, j))
-        
+                    res = max(res, dfs(i, j))
         return res
-        
-        

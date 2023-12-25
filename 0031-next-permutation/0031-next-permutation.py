ad29b8ca3319e53f637ec720c0660506f546bd2d
Jiangtian Han <112032591+jiangtianh@users.noreply.github.com>
2023-12-24 19:43:30 -0800

    Time: 41 ms (90.13%), Space: 17.4 MB (5.03%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -2,6 +2,10 @@ class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
+        
+        First find the pivot from right to left (the point where the rest of the array is decreasing) for example: [1, 6, 4, 2, 1], so 1 will be the pivot index 
+        Then, from right to left, find the first number that is bigger than pivot 
+        swap the second number with the pivot number, and reverse the portion to the right of the pivot index 
         """
         def reverse(l, r):
             while l < r:

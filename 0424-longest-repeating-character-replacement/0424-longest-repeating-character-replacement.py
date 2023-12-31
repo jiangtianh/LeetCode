f615a502070f7e0601855adb8f860154cf2afd49
Jiangtian Han <112032591+jiangtianh@users.noreply.github.com>
2023-12-31 14:23:01 -0800

    Time: 254 ms (9.52%), Space: 17.5 MB (6.60%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,23 +1,24 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        d = collections.defaultdict(int)
         
-        l = r = 0
-        res = 0
-        currentMax = 1
+        d = {}
+        l = 0
+        res = 1
         
-        while r < len(s):
-            c = s[r]
-            d[c] += 1
-            currentMax = max(currentMax, d[c])
-            while (r - l + 1) - currentMax > k:
+        for r, c in enumerate(s):
+            d[c] = d.get(c, 0) + 1
+            
+            
+            most = max(d.values())
+            
+            while (r - l + 1) - most > k:
                 d[s[l]] -= 1
                 l += 1
             
-            res = max(r - l + 1, res)
-            r += 1
+            res = max(res, r - l + 1)
+            
+            
+            
+            
             
         return res
-        
-        
-        

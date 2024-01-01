b6e8a33dd11362678eede7d2108af7cf5bd1c8df
Jiangtian Han <112032591+jiangtianh@users.noreply.github.com>
2024-01-01 01:22:05 -0800

    Time: 130 ms (92.82%), Space: 18.8 MB (16.28%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -6,8 +6,7 @@ class Solution:
         p = 0
         for i in range(len(nums)):
             if nums[i] != 0:
-                nums[p] = nums[i]
+                nums[p], nums[i] = nums[i], nums[p]
                 p += 1
                 
-        for i in range(p, len(nums)):
-            nums[i] = 0
+       

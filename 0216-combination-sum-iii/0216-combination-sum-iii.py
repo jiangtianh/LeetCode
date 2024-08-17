b831b51dbaea2d287b475ce0de2d894729b23e66
Jiangtian Han <112032591+jiangtianh@users.noreply.github.com>
2024-08-17 16:04:06 -0700

    Time: 41 ms (23.45%), Space: 16.5 MB (29.72%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.py
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -1,21 +1,22 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
         self.res = []
+        self.temp = []
+        self.total = 0
+
+        def f(j):
+            if self.total > n:
+                return 
+            elif self.total == n and len(self.temp) == k:
+                self.res.append(self.temp[:])
+                return 
+            
+            for i in range(j, 10):
+                self.total += i
+                self.temp.append(i)
+                f(i+1)
+                self.temp.pop()
+                self.total -= i
         
-        def f(start, left, k, temp):
-            if k == 0:
-                if left == 0:
-                    self.res.append(temp[:])
-                return 
-            if start >= 10:
-                return 
-            
-            temp.append(start)
-            f(start + 1, left - start, k - 1, temp)
-            temp.pop() 
-            
-            f(start + 1, left, k, temp)
-            
-        f(1, n, k, [])
-    
+        f(1)
         return self.res

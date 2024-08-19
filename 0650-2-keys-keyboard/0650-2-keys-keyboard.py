97dade4be2026b75036311f7fb2480ad713566d2
Jiangtian Han <112032591+jiangtianh@users.noreply.github.com>
2024-08-18 17:17:05 -0700

    Time: 43 ms (70.73%), Space: 16.5 MB (61.96%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.py
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.py
@@ -1,15 +1,12 @@
 class Solution:
     def minSteps(self, n: int) -> int:
 
-        self.d = {}
 
-        
+
+        @cache
         def f(n):
             if n == 1:
                 return 0
-
-            if n in self.d:
-                return self.d[n]
             res = math.inf 
             
             for i in range(2, (n // 2) + 1):
@@ -17,11 +14,9 @@ class Solution:
                     res = min(res, f(n//i) + i)
             
             if res == math.inf:
-                self.d[n] = n
                 return n
-            else:
-                self.d[n] = res
-                return res
+
+            return res
             
 
         return f(n)

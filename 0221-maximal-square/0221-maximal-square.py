7d456d23fbb33a432c8b126a580e42eb926139ca
Jiangtian Han <112032591+jiangtianh@users.noreply.github.com>
2024-08-28 15:54:22 -0700

    Time: 522 ms (68.13%), Space: 19.3 MB (82.94%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.py
+++ 0221-maximal-square/0221-maximal-square.py
@@ -1,35 +1,22 @@
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
-        self.res = 0
-        
-        @cache
-        def f(x, y):
-            if x not in range(len(matrix)) or y not in range(len(matrix[0])):
-                return 0
-            
-            bot = f(x + 1, y)
-            right = f(x, y + 1)
-            botright = f(x + 1, y + 1)
-            
-            if bot and right and botright:
-                minimum = min(bot, right, botright)
-                if matrix[x][y] == '1':
-                    res = minimum + 1
-                else:
-                    res = 0
-            else:
-                res = int(matrix[x][y])
-            
-            self.res = max(self.res, res ** 2)
-            return res
+        m, n = len(matrix), len(matrix[0])
 
+        dp = [[0] * n for _ in range(m)]
+        res = 0
+
+        for i in range(m):
+            if matrix[i][-1] == "1":
+                res = 1
+            dp[i][-1] = int(matrix[i][-1])
+        for i in range(n):
+            if matrix[-1][i] == "1":
+                res = 1
+            dp[-1][i] = int(matrix[-1][i])
         
-        f(0, 0)
-        return self.res
-            
-        
-            
-            
-            
-            
-            
+        for x in range(m-2, -1, -1):
+            for y in range(n-2, -1, -1):
+                if matrix[x][y] == '1':
+                    dp[x][y] = min(dp[x+1][y+1], dp[x+1][y], dp[x][y+1]) + 1
+                    res = max(res, dp[x][y], 1)
+        return res ** 2

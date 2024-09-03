cef142e72d5777b82ddeddb9c84d9098ba5447af
Jiangtian Han <112032591+jiangtianh@users.noreply.github.com>
2024-09-03 14:14:43 -0700

    Time: 590 ms (54.31%), Space: 20.2 MB (81.33%) - LeetHub

---
+++ 1558-course-schedule-iv/1558-course-schedule-iv.py
@@ -0,0 +1,30 @@
+class Solution:
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        if not prerequisites:
+            return [False] * len(queries)
+        graph = collections.defaultdict(list)
+        for x, y in prerequisites:
+            graph[y].append(x)
+    
+        def f(i, visited):
+            for nei in graph[i]:
+                if nei not in visited:
+                    visited.add(nei)
+                    f(nei, visited)
+        
+        d = {}
+
+        for i in range(numCourses):
+            visited = set()
+            f(i, visited)
+            d[i] = visited
+        
+        res = []
+        for x, y in queries:
+            if x in d[y]:
+                res.append(True)
+            else:
+                res.append(False)
+        return res
+
+

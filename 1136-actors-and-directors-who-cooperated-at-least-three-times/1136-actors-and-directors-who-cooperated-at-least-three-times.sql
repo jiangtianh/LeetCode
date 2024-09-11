7277cdf6b03ca908d04a1ee923b8b9f1314458e3
Jiangtian Han <112032591+jiangtianh@users.noreply.github.com>
2024-09-11 10:53:03 -0700

    Time: 344 ms (70.44%), Space: 0B (100%) - LeetHub

--- 1136-actors-and-directors-who-cooperated-at-least-three-times/1136-actors-and-directors-who-cooperated-at-least-three-times.sql
+++ 1136-actors-and-directors-who-cooperated-at-least-three-times/1136-actors-and-directors-who-cooperated-at-least-three-times.sql
@@ -1,8 +1,6 @@
 # Write your MySQL query statement below
 
 SELECT actor_id, director_id 
-FROM
-    (SELECT COUNT(timestamp) AS c, actor_id, director_id
-    FROM ActorDirector
-    GROUP BY actor_id, director_id) AS sub
-WHERE c >= 3
+FROM ActorDirector
+GROUP BY actor_id, director_id
+HAVING COUNT(timestamp) >= 3

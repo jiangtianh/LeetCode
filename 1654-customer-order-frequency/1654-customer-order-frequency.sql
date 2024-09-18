ab787c779392a13c406b7bb832fa9168d4bf131a
Jiangtian Han <112032591+jiangtianh@users.noreply.github.com>
2024-09-18 00:49:12 -0700

    Time: 706 ms (98.93%), Space: 0B (100%) - LeetHub

--- 1654-customer-order-frequency/1654-customer-order-frequency.sql
+++ 1654-customer-order-frequency/1654-customer-order-frequency.sql
@@ -1,20 +1,16 @@
-# Write your MySQL query statement below
-
-SELECT g.customer_id, name
-FROM (
-    SELECT 
-        o.customer_id, SUM(o.quantity * p.price) AS total_spent,
-        CASE 
-            WHEN o.order_date BETWEEN "2020-06-01" AND "2020-06-30" THEN "June"
-            WHEN o.order_date BETWEEN "2020-07-01" AND "2020-07-31" THEN "July"
-        END AS order_month
-    FROM Orders o
-    JOIN Product p
-    ON o.product_id = p.product_id
-    WHERE o.order_date BETWEEN "2020-06-01" AND "2020-07-31"
-    GROUP BY customer_id, order_month
-) AS g 
-JOIN Customers c ON g.customer_id = c.customer_id
-GROUP BY g.customer_id
-HAVING SUM(IF(g.order_month = 'June', total_spent, 0)) >= 100
-   AND SUM(IF(g.order_month = 'July', total_spent, 0)) >= 100;
+SELECT
+    customer_id, name
+FROM 
+    Customers
+    JOIN Orders USING(customer_id)
+    JOIN Product Using(product_id)
+WHERE YEAR(order_date) = 2020
+GROUP BY customer_id
+HAVING 
+    SUM(
+        IF(MONTH(order_date) = 6, quantity, 0) * price
+    ) >= 100 
+    AND
+    SUM(
+        IF(Month(order_date) = 7, quantity, 0) * price
+    ) >= 100

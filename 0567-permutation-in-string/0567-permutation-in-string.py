ee4f850164b45d6877404345b7c10b39bc5c22f1
Jiangtian Han <112032591+jiangtianh@users.noreply.github.com>
2024-10-04 17:08:43 -0700

    Time: 65 ms (52.15%), Space: 16.7 MB (22.1%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,27 +1,31 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s1) > len(s2):
-            return False
-        counter = collections.Counter(s1)
-        have = {}
-        
-        for i in range(len(s1)):
-            have[s2[i]] = have.get(s2[i], 0) + 1
-    
-        l = 0
-        for i in range(len(s1), len(s2)):
-            if have == counter:
-                return True
-            have[s2[i]] = have.get(s2[i], 0) + 1
-            
-            have[s2[l]] -= 1
-            if have[s2[l]] == 0:
-                have.pop(s2[l])
-            l += 1
-        
-        return have == counter
-            
-        
-        
-        
-        
+        l = 0   
+        d = collections.Counter(s1)        
+
+        need = len(s1)
+
+        for r in range(len(s2)):
+            c = s2[r]
+            if c in d and d[c] > 0:
+                need -= 1
+                d[c] -= 1
+            elif c in d:
+                while s2[l] != c:
+                    if s2[l] in d:
+                        d[s2[l]] += 1
+                        need += 1
+                    l += 1
+                l += 1
+            else:
+                while l <= r:
+                    lc = s2[l]
+                    if lc in d:
+                        d[lc] += 1
+                        need += 1
+                    l += 1
+                    
+            if need == 0:
+                return True 
+
+        return False

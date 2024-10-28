e515ba39a0a50f47db48a386a4db9a1e93801ea5
Jiangtian Han <112032591+jiangtianh@users.noreply.github.com>
2024-10-27 17:16:54 -0700

    Time: 382 ms (5.41%), Space: 109.8 MB (5.33%) - LeetHub

--- 2586-longest-square-streak-in-an-array/2586-longest-square-streak-in-an-array.py
+++ 2586-longest-square-streak-in-an-array/2586-longest-square-streak-in-an-array.py
@@ -2,16 +2,14 @@ class Solution:
     def longestSquareStreak(self, nums: List[int]) -> int:
         nums.sort()
         self.res = 1
-        print(nums)
+
         @cache
         def f(i):
             if i >= len(nums):
                 return 0
             num = nums[i]
             square = num ** 2
-
             idx = bisect_left(nums, square, i+1)
-            print("i:" ,i, 'idx:', idx)
             if idx >= len(nums) or nums[idx] != square:
                 return 1
             else:

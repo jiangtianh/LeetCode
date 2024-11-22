9b5638204235554dd27e61f151b42683fdca7a0b
Jiangtian Han <112032591+jiangtianh@users.noreply.github.com>
2024-11-22 11:05:30 -0800

    Time: 56 ms (65.08%), Space: 19.9 MB (18.53%) - LeetHub

---
+++ 1634-clone-n-ary-tree/1634-clone-n-ary-tree.py
@@ -0,0 +1,24 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children if children is not None else []
+"""
+
+class Solution:
+    def cloneTree(self, root: 'Node') -> 'Node':
+    
+        def f(node):
+            if not node:
+                return None
+            newNode = Node(node.val)
+            for child in node.children:
+                newNode.children.append(f(child))
+            
+            return newNode
+
+        return f(root)
+
+
+

2cd8a8007921376e9b77ebe3f460d88db4bc7c41
Jiangtian Han <112032591+jiangtianh@users.noreply.github.com>
2024-11-22 10:48:41 -0800

    Time: 40 ms (56.04%), Space: 17.2 MB (7.16%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -9,28 +9,22 @@ class Node:
 from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        
-        
-        cloned = {}
-        
-        
-        def dfs(node):
-                
-            newNode = Node(node.val)
-            cloned[node] = newNode 
-                
-            for neighbor in node.neighbors:
-                if neighbor not in cloned:
-                    newNode.neighbors.append(dfs(neighbor))
-                else:
-                    newNode.neighbors.append(cloned[neighbor])
-            
-            return newNode 
-    
         if not node:
-            return None 
-        return dfs(node)
-            
-        
-        
-        
+            return None
+
+        self.d = {}
+
+        def f(node):
+            if node in self.d:
+                return self.d[node]
+            else:
+                newNode = Node(node.val)
+                self.d[node] = newNode
+                for neighbor in node.neighbors:
+                    newNode.neighbors.append(f(neighbor))
+                return newNode
+
+        return f(node)        
+
+
+

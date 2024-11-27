c36912b6656559e559320fc1bce063b83165229c
Jiangtian Han <112032591+jiangtianh@users.noreply.github.com>
2024-11-27 11:07:14 -0800

    Time: 0 ms (100%), Space: 17.2 MB (9.74%) - LeetHub

--- 1446-angle-between-hands-of-a-clock/1446-angle-between-hands-of-a-clock.py
+++ 1446-angle-between-hands-of-a-clock/1446-angle-between-hands-of-a-clock.py
@@ -3,9 +3,8 @@ class Solution:
         minutesDegree = 6 * minutes
         hourDegree = (hour % 12) * 30 + (minutes / 60) * 30
 
-        print("minutes: ", minutes)
-        print("hours: ", hourDegree)
+
         
         diff = abs(hourDegree - minutesDegree)
-        print("diff: ", diff)
+
         return min(diff, 360 - diff)

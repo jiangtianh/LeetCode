693f6af9ac235d4a11e28e0614955d027b4f73b9
Jiangtian Han <112032591+jiangtianh@users.noreply.github.com>
2024-12-03 23:07:34 -0800

    Time: 74 ms (52.43%), Space: 36.1 MB (7%) - LeetHub

--- 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.py
+++ 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.py
@@ -1,20 +1,20 @@
 class Solution:
     def canArrange(self, arr: List[int], k: int) -> bool:
-        counter = collections.defaultdict(int)
-
+        d = collections.defaultdict(int)
         for n in arr:
-            remainder = n % k
-            counter[remainder] += 1
-        
+            mod = n % k 
+            d[mod] += 1
 
-        if counter[0] % 2 != 0:
+
+        if d[0] % 2 != 0:
             return False
 
-        for i in range(1, k // 2 + 1):
-            if counter[i] != counter[k-i]:
+        for i in range(1, k//2 + 1):
+            
+            if d[i] != d[k-i]:
+
                 return False
 
-        if k % 2 == 0 and counter[k//2] % 2 != 0:
-            return False
+        
         
         return True

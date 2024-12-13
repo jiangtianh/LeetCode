aabeb2ab909fa4b8d798c8a41094441ef604349f
Jiangtian Han <112032591+jiangtianh@users.noreply.github.com>
2024-12-13 14:41:44 -0800

    Time: 294 ms (49.18%), Space: 20.4 MB (7.79%) - LeetHub

--- 3425-maximum-number-of-potholes-that-can-be-fixed/3425-maximum-number-of-potholes-that-can-be-fixed.py
+++ 3425-maximum-number-of-potholes-that-can-be-fixed/3425-maximum-number-of-potholes-that-can-be-fixed.py
@@ -1,20 +1,14 @@
 class Solution:
     def maxPotholes(self, road: str, budget: int) -> int:
-        li = []
-        i = 0
-        
         road = road.split(".")
         road.sort(key=lambda x: -len(x))
         
         res = 0
-        for i, r in enumerate(road):
-            if len(r) == 0:
-                continue
+        for r in road:
+            if len(r) == 0 or budget == 0:
+                break
             numToFix = min(budget-1, len(r))
             res += numToFix
             budget -= numToFix + 1
-
-            if budget == 0:
-                break
 
         return res

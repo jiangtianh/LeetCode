4f0c09ce98b491138d5f07d7d0ab62eac69c0bf0
Jiangtian Han <112032591+jiangtianh@users.noreply.github.com>
2024-12-26 09:45:37 +0800

    Time: 149 ms (65.7%), Space: 45.2 MB (5.57%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,17 +1,11 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        
         @cache
-        def f(i, amount):
+        def f(i, cur):
             if i == len(nums):
-                if amount == target:
+                if cur == target:
                     return 1
                 else:
                     return 0
-            else:
-                return f(i + 1, amount + nums[i]) + f(i + 1, amount - nums[i])
-        
+            return f(i+1, cur + nums[i]) + f(i+1, cur - nums[i])            
         return f(0, 0)
-        
-
-

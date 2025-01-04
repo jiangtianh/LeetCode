9eb33bb7a07f2e92d760cf284399769d718ad314
Jiangtian Han <112032591+jiangtianh@users.noreply.github.com>
2025-01-05 04:39:28 +0800

    Time: 12 ms (70.65%), Space: 18.1 MB (29.78%) - LeetHub

---
+++ 1176-design-a-leaderboard/1176-design-a-leaderboard.py
@@ -0,0 +1,24 @@
+class Leaderboard:
+
+    def __init__(self):
+        self.score = collections.defaultdict(int)
+
+    def addScore(self, playerId: int, score: int) -> None:
+        self.score[playerId] += score        
+
+    def top(self, K: int) -> int:
+        s = list(self.score.values())
+        s.sort(reverse=True)
+        total = 0
+        for i in range(K):
+            total += s[i]
+        return total
+    def reset(self, playerId: int) -> None:
+        self.score[playerId] = 0        
+
+
+# Your Leaderboard object will be instantiated and called as such:
+# obj = Leaderboard()
+# obj.addScore(playerId,score)
+# param_2 = obj.top(K)
+# obj.reset(playerId)

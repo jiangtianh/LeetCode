0a833b3309b8c866eecf6ae9f614cecaaacb567c
Jiangtian Han <112032591+jiangtianh@users.noreply.github.com>
2025-01-08 06:03:41 +0800

    Time: 79 ms (74.7%), Space: 18.9 MB (18.63%) - LeetHub

--- 1126-minimum-cost-to-connect-sticks/1126-minimum-cost-to-connect-sticks.py
+++ 1126-minimum-cost-to-connect-sticks/1126-minimum-cost-to-connect-sticks.py
@@ -1,12 +1,9 @@
 class Solution:
     def connectSticks(self, sticks: List[int]) -> int:
         heapq.heapify(sticks)
-
         res = 0
         while len(sticks) > 1:
-            left = heapq.heappop(sticks)
-            right = heapq.heappop(sticks)
-            heapq.heappush(sticks, left + right)
-            res += left + right
-
+            n = heapq.heappop(sticks) + heapq.heappop(sticks)
+            res += n
+            heapq.heappush(sticks, n)
         return res

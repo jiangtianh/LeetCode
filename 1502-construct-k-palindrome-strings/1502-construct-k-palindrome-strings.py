68fb3db1a0ff710255afd71ab7bec311bebe9901
Jiangtian Han <112032591+jiangtianh@users.noreply.github.com>
2025-01-11 08:20:04 +0800

    Time: 31 ms (71.37%), Space: 18 MB (34.36%) - LeetHub

--- 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.py
+++ 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.py
@@ -2,11 +2,13 @@ class Solution:
     def canConstruct(self, s: str, k: int) -> bool:
         if len(s) < k:
             return False
-        counter = collections.Counter(s)
-        haveToHave = 0
 
+        counter = collections.Counter(s)
+
+        oddCount = 0
         for c in counter:
             if counter[c] % 2 == 1:
-                haveToHave += 1
-        
-        return haveToHave <= k
+                oddCount += 1
+                
+ 
+        return oddCount <= k

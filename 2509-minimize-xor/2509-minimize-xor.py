ae2668dc28861183bcc4d4e0df324fd32cc95681
Jiangtian Han <112032591+jiangtianh@users.noreply.github.com>
2025-01-14 16:20:56 -0800

    Time: 3 ms (18.4%), Space: 17.8 MB (41.6%) - LeetHub

---
+++ 2509-minimize-xor/2509-minimize-xor.py
@@ -0,0 +1,28 @@
+class Solution:
+    def minimizeXor(self, num1: int, num2: int) -> int:
+        num1 = bin(num1)[2:]
+        num2 = bin(num2)[2:]
+        count = num2.count("1")
+        print("num1: ", num1)
+        print("num2: ", num2)
+        print(count)
+        
+        if count >= len(num1):
+            return int("1" * count, 2)
+        else:
+            n = len(num1)
+            res = ["0"] * n
+            for i in range(n):
+                if num1[i] == "1":
+                    res[i] = "1"
+                    count -= 1
+                    if count == 0:
+                        break
+            i = n - 1
+            while i >= 0 and count > 0:
+                if res[i] == "0":
+                    res[i] = "1"
+                    count -= 1
+                i -= 1
+                
+            return int("".join(res), 2)

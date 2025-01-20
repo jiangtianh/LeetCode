a01d73d38bf6684e36b690cfd2016e84c7567dfc
Jiangtian Han <112032591+jiangtianh@users.noreply.github.com>
2025-01-19 16:15:45 -0800

    Time: 49 ms (90.55%), Space: 24.4 MB (82.4%) - LeetHub

---
+++ 0550-shortest-path-to-get-food/0550-shortest-path-to-get-food.py
@@ -0,0 +1,30 @@
+class Solution:
+    def getFood(self, grid: List[List[str]]) -> int:
+        step = 0
+        m, n = len(grid), len(grid[0])
+        dirs = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        q = collections.deque()
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == "*":
+                    q.append((i, j))
+                    break
+            if len(q) == 1:
+                break
+        print(q)
+        while q:
+            for _ in range(len(q)):
+                x, y = q.popleft()
+
+                for i, j in dirs:
+                    newX, newY = x + i, y + j
+                    if 0 <= newX < m and 0 <= newY < n:
+                        if grid[newX][newY] == "#":
+                            return step + 1
+                        elif grid[newX][newY] == "O":
+                            q.append((newX, newY))
+                            grid[newX][newY] = "*"
+
+            step += 1
+
+        return -1

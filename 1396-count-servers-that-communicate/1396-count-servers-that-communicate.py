206705d19f6164ea9f6d34f7ef302119a913cb56
Jiangtian Han <112032591+jiangtianh@users.noreply.github.com>
2025-01-22 16:14:46 -0800

    Time: 21 ms (38.95%), Space: 19.5 MB (60.67%) - LeetHub

--- 1396-count-servers-that-communicate/1396-count-servers-that-communicate.py
+++ 1396-count-servers-that-communicate/1396-count-servers-that-communicate.py
@@ -1,24 +1,15 @@
 class Solution:
     def countServers(self, grid: List[List[int]]) -> int:
-        visited = set()
         m, n = len(grid), len(grid[0])
-
-        def dfs(x, y):
-            visited.add((x, y))
-            res = 1
-            for i in range(m):
-                if grid[i][y] == 1 and (i, y) not in visited:
-                    res += dfs(i, y)
-            for i in range(n):
-                if grid[x][i] == 1 and (x, i) not in visited:
-                    res += dfs(x, i)
-            return res
+        row, col = [0] * m, [0] * n
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    row[i] += 1
+                    col[j] += 1
         res = 0
-        for x in range(m):
-            for y in range(n):
-                if grid[x][y] == 1 and (x, y) not in visited:
-                    p = dfs(x, y)
-                    if p != 1:
-                        res += p
-
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1 and (row[i] > 1 or col[j] > 1):
+                    res += 1
         return res

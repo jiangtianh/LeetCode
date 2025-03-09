bd61c385d4647e722142b30775c5300fb32d8824
Jiangtian Han <112032591+jiangtianh@users.noreply.github.com>
2025-03-08 16:06:57 -0800

    Time: 707 ms (81.06%), Space: 21.6 MB (39.54%) - LeetHub

--- 3483-alternating-groups-ii/3483-alternating-groups-ii.py
+++ 3483-alternating-groups-ii/3483-alternating-groups-ii.py
@@ -2,7 +2,6 @@ class Solution:
     def numberOfAlternatingGroups(self, colors: List[int], k: int) -> int:
         l = 0
         colors = colors + colors[:k-1]
-        print(colors)
         res = 0
         for r in range(1, len(colors)):
             if colors[r] == colors[r-1]:

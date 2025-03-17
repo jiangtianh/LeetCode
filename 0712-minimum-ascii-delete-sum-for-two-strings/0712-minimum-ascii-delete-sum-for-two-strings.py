ac6070fbc49b3d2eb0cac38fc4ee5200c0a2f996
Jiangtian Han <112032591+jiangtianh@users.noreply.github.com>
2025-03-17 11:10:49 -0700

    Time: 377 ms (45.11%), Space: 22 MB (64.43%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.py
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.py
@@ -1,29 +1,15 @@
 class Solution:
     def minimumDeleteSum(self, s1: str, s2: str) -> int:
-        @cache
-        def f(i, j):
-            if i == len(s1) and j == len(s2):
-                return 0
-            elif i == len(s1):
-                return sum([ord(c) for c in s2[j:]])
-            elif j == len(s2):
-                return sum([ord(c) for c in s1[i:]])
-            res = math.inf
-            if s1[i] == s2[j]:
-                res = min(res, f(i+1, j+1))
-            
-            res = min(res, f(i+1, j) + ord(s1[i]), f(i, j+1) + ord(s2[j]))
-            return res
-        return f(0, 0)
+        
 
         dp = [[math.inf] * (len(s2) + 1) for _ in range(len(s1) + 1)]
-        for i in range(len(s1)):
-            dp[i][-1] = 0
-        for j in range(len(s2)):
-            dp[-1][j] = 0
+        dp[-1][-1] = 0
+        for i in range(len(s1) - 1, -1, -1):
+            dp[i][-1] = dp[i+1][-1] + ord(s1[i])
+        for j in range(len(s2) - 1, -1, -1):
+            dp[-1][j] = dp[-1][j+1] + ord(s2[j])
         for i in range(len(s1) - 1, -1, -1):
             for j in range(len(s2) - 1, -1, -1):
-                dp[i][j] = math.inf
                 if s1[i] == s2[j]:
                     dp[i][j] = min(dp[i][j], dp[i+1][j+1])
                 dp[i][j] = min(dp[i][j], dp[i][j+1] + ord(s2[j]), dp[i+1][j] + ord(s1[i]))

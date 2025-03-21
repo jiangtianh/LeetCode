8dd1f57b33dea771cbb78c5e41af92bfd69bfb99
Jiangtian Han <112032591+jiangtianh@users.noreply.github.com>
2025-03-21 10:20:12 -0700

    Time: 131 ms (60.4%), Space: 58.1 MB (11.72%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -1,35 +1,35 @@
 class RandomizedSet:
-
     def __init__(self):
         self.li = []
-        self.d = {}
-        self.size = 0
+        self.numToIdx = {}
         
 
     def insert(self, val: int) -> bool:
-        if val in self.d:
-            return False 
-        else:
-            self.d[val] = self.size
-            self.size += 1
+        if val not in self.numToIdx:
+            self.numToIdx[val] = len(self.li)
             self.li.append(val)
-            return True 
-        
+            return True
+        return False
+
     def remove(self, val: int) -> bool:
-        if val not in self.d:
-            return False 
-        else:
-            self.li[self.d[val]] = self.li[-1]
-            self.d[self.li[-1]] = self.d[val]
-            self.d.pop(val)
+        if val not in self.numToIdx:
+            return False
+        if len(self.li) == 1:
+            self.numToIdx.pop(val)
             self.li.pop()
-            self.size -= 1
-            return True 
-        
-        
+        else:
+            lastVal = self.li[-1]
+            curIdx = self.numToIdx[val]
+            self.li[curIdx] = lastVal
+            self.numToIdx[lastVal] = curIdx
+            self.li.pop()
+            self.numToIdx.pop(val)
+
+        return True
+
 
     def getRandom(self) -> int:
-        return self.li[random.randint(0, self.size-1)]
+        return self.li[randint(0, len(self.li) - 1)]  
 
 
 # Your RandomizedSet object will be instantiated and called as such:

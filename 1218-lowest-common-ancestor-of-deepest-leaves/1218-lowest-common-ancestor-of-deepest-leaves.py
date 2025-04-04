a2ce76d08a67862218e47304382e46e12fecdac6
Jiangtian Han <112032591+jiangtianh@users.noreply.github.com>
2025-04-03 17:13:50 -0700

    Time: 5 ms (27.61%), Space: 18.2 MB (71.52%) - LeetHub

--- 1218-lowest-common-ancestor-of-deepest-leaves/1218-lowest-common-ancestor-of-deepest-leaves.py
+++ 1218-lowest-common-ancestor-of-deepest-leaves/1218-lowest-common-ancestor-of-deepest-leaves.py
@@ -6,32 +6,21 @@
 #         self.right = right
 class Solution:
     def lcaDeepestLeaves(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        def isLeaf(node):
-            return not node.left and not node.right 
-        
-        def findDeepest(node, depth):
-            if isLeaf(node):
-                return depth
-            left = findDeepest(node.left, depth + 1) if node.left else 0
-            right = findDeepest(node.right, depth + 1) if node.right else 0
-            return max(left, right)
-
-        self.deepest = findDeepest(root, 0)
-        self.res = root
+        self.deepest = 0
+        self.res = None
 
         def f(node, depth):
             if not node:
-                return False
-            if depth == self.deepest:
+                return depth - 1
+            
+            left = f(node.left, depth + 1)
+            right = f(node.right, depth + 1)
+            if left == right and left >= self.deepest:
+                self.deepest = left
                 self.res = node
-                return True 
-            left = f(node.left, depth+1)
-            right = f(node.right, depth+1)
-
-            if left and right:
-                self.res = node
-            return left or right 
-
+            return max(left, right)
+        
         f(root, 0)
         return self.res
+
             

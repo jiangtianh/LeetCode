c035a3b04c1e6faf0278188bed2f8fc6c7b92726
Jiangtian Han <112032591+jiangtianh@users.noreply.github.com>
2025-04-17 17:07:47 -0700

    Time: 3 ms (97.1%), Space: 17.8 MB (61.49%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.py
+++ 0038-count-and-say/0038-count-and-say.py
@@ -1,21 +1,24 @@
 class Solution:
     def countAndSay(self, n: int) -> str:
-        initial = [1]
+        
+        def f(n):
+            if n == 1:
+                return "1"
+            res = []
+            prevRes = f(n-1)
 
-        for _ in range(n-1):
-            newLi = []
-            prev = initial[0]
-            count = 1
-            for i in range(1, len(initial)):
-                if prev == initial[i]:
+            prevC = prevRes[0]
+            count = 0
+
+            for c in prevRes:
+                if c == prevC:
                     count += 1
                 else:
-                    newLi.append(count)
-                    newLi.append(prev)
-                    prev = initial[i]
+                    res.append(str(count))
+                    res.append(prevC)
+                    prevC = c
                     count = 1
-            newLi.append(count)
-            newLi.append(prev)
-            initial = newLi
-
-        return "".join([str(n) for n in initial])
+            res.append(str(count))
+            res.append(prevC)
+            return "".join(res)
+        return f(n)

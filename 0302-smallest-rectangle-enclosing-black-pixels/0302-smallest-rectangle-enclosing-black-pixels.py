40a9b7f58352a24f5e9b83522445f49248bb5ac9
Jiangtian Han <112032591+jiangtianh@users.noreply.github.com>
2025-04-21 17:21:56 -0700

    Time: 79 ms (14.81%), Space: 19.6 MB (51.23%) - LeetHub

---
+++ 0302-smallest-rectangle-enclosing-black-pixels/0302-smallest-rectangle-enclosing-black-pixels.py
@@ -0,0 +1,31 @@
+class Solution:
+    def minArea(self, image: List[List[str]], x: int, y: int) -> int:
+        smallestX = x
+        smallestY = y
+        biggestX = x
+        biggestY = y
+        m, n = len(image), len(image[0])
+
+        dirs = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        
+        q = collections.deque()
+        q.append((x, y))
+        visited = set()
+
+        while q:
+            x, y = q.popleft()
+            if (x, y) in visited:
+                continue
+            biggestX = max(biggestX, x)
+            biggestY = max(biggestY, y)
+            smallestX = min(smallestX, x)
+            smallestY = min(smallestY, y)
+            visited.add((x, y))
+            for i, j in dirs:
+                newX, newY = x + i, y + j
+                if 0 <= newX < m and 0 <= newY < n and image[newX][newY] == "1":
+                    q.append((newX, newY))
+
+        print(biggestX, smallestX, biggestY, smallestY)
+        return (biggestX - smallestX + 1) * (biggestY - smallestY + 1)
+            

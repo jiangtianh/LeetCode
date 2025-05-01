4a2ac79ae50a8f61272aa0c6e6366a376434d160
Jiangtian Han <112032591+jiangtianh@users.noreply.github.com>
2025-05-01 13:46:44 -0700

    Time: 114 ms (89.24%), Space: 33 MB (76.47%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.py
+++ 0221-maximal-square/0221-maximal-square.py
@@ -1,22 +1,15 @@
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
-        m, n = len(matrix), len(matrix[0])
+        n, m = len(matrix), len(matrix[0])
+        dp = [[0] * (m + 1) for _ in range(n + 1)]
 
-        dp = [[0] * n for _ in range(m)]
         res = 0
 
-        for i in range(m):
-            if matrix[i][-1] == "1":
-                res = 1
-            dp[i][-1] = int(matrix[i][-1])
-        for i in range(n):
-            if matrix[-1][i] == "1":
-                res = 1
-            dp[-1][i] = int(matrix[-1][i])
-        
-        for x in range(m-2, -1, -1):
-            for y in range(n-2, -1, -1):
-                if matrix[x][y] == '1':
-                    dp[x][y] = min(dp[x+1][y+1], dp[x+1][y], dp[x][y+1]) + 1
-                    res = max(res, dp[x][y], 1)
-        return res ** 2
+        for i in range(n-1, -1, -1):
+            for j in range(m-1, -1, -1):
+                if matrix[i][j] == "1":
+                    temp = min(dp[i+1][j], dp[i][j+1], dp[i+1][j+1]) + 1
+                    res = max(res, temp)
+                    dp[i][j] = temp
+
+        return res * res

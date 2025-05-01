f597e8cb9cb30496064cf11c4ca6dca0d7a20025
Jiangtian Han <112032591+jiangtianh@users.noreply.github.com>
2025-05-01 12:30:09 -0700

    Time: 205 ms (19.59%), Space: 19.4 MB (68.14%) - LeetHub

--- 0353-design-snake-game/0353-design-snake-game.py
+++ 0353-design-snake-game/0353-design-snake-game.py
@@ -1,38 +1,41 @@
 class SnakeGame:
 
     def __init__(self, width: int, height: int, food: List[List[int]]):
-        self.height = height
-        self.width = width
-        self.snake = collections.deque()
-        self.snake.append((0, 0))
-        self.body = set()
-        self.body.add((0, 0))
-        self.dirs = {"R": (0, 1), "D": (1, 0), "L": (0, -1), "U": (-1, 0)}        
-        self.food = food
-        self.foodIdx = 0
-        self.score = 0
-        
+        self.snake = collections.deque([(0, 0)])
+        self.body = {(0, 0)}
+        self.food = collections.deque(food)
+        self.m = width
+        self.n = height
+        self.dirs = {"R": [0, 1], "L": [0, -1], "D": [1, 0], "U": [-1, 0]}
 
+    def check(self, position):
+        x, y = position
+        return 0 <= x < self.n and 0 <= y < self.m and (x, y) not in self.body
 
     def move(self, direction: str) -> int:
-        newX, newY = self.snake[-1][0] + self.dirs[direction][0], self.snake[-1][1] + self.dirs[direction][1]
-        
-        tailX, tailY = self.snake.popleft()
-        self.body.remove((tailX, tailY))
-        
-        if not (0 <= newX < self.height and 0 <= newY < self.width and (newX, newY) not in self.body):
+        i, j = self.dirs[direction]
+        newPosition = (self.snake[-1][0] + i, self.snake[-1][1] + j)
+
+        scored = self.food and newPosition[0] == self.food[0][0] and newPosition[1] == self.food[0][1]
+
+        if not scored:
+            tailPosition = self.snake.popleft()
+            self.body.remove(tailPosition)
+         
+        if not self.check(newPosition):
             return -1
-        elif self.foodIdx < len(self.food) and newX == self.food[self.foodIdx][0] and newY == self.food[self.foodIdx][1]:
-            self.snake.appendleft((tailX, tailY)) 
-            self.body.add((tailX, tailY))
-            self.score += 1
-            self.foodIdx += 1
-            
 
-        self.snake.append((newX, newY))
-        self.body.add((newX, newY))
+        self.snake.append(newPosition)
+        self.body.add(newPosition)
+        
 
-        return self.score
+        if scored:
+            self.food.popleft()
+
+        return len(self.body) - 1
+        
+
+
 
 # Your SnakeGame object will be instantiated and called as such:
 # obj = SnakeGame(width, height, food)

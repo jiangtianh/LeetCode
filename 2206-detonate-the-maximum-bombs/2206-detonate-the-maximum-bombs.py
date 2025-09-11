6d3c5588f1b092cb50831885ea60860495a7e354
Jiangtian Han <112032591+jiangtianh@users.noreply.github.com>
2025-09-11 12:19:38 -0700

    Time: 326 ms (68.06%), Space: 18.3 MB (46.22%) - LeetHub

--- 2206-detonate-the-maximum-bombs/2206-detonate-the-maximum-bombs.py
+++ 2206-detonate-the-maximum-bombs/2206-detonate-the-maximum-bombs.py
@@ -1,19 +1,37 @@
 class Solution:
     def maximumDetonation(self, bombs: List[List[int]]) -> int:
-        def getDistance(ax, ay, bx, by):
-            return math.sqrt(abs(ax - bx) ** 2 + abs(ay - by) ** 2)
+        def getDist(i ,j):
+            x1, y1, r1 = bombs[i]
+            x2, y2, r2 = bombs[j]
+            return sqrt(abs(x1 - x2) ** 2 + abs(y1 - y2) ** 2)
+            
+
+        graph = collections.defaultdict(list)
+
+        for i in range(len(bombs)):
+            r1 = bombs[i][2]
+            for j in range(i+1, len(bombs)):
+                r2 = bombs[j][2]
+                dist = getDist(i ,j)
+                if dist <= r1:
+                    graph[i].append(j)
+                if dist <= r2:
+                    graph[j].append(i)
+
+        visited = set()
 
         def dfs(i, visited):
             res = 1
-            for j in range(len(bombs)):
-                bomb = bombs[j]
-                if j not in visited and bombs[i][2] >= getDistance(bombs[i][0], bombs[i][1], bomb[0], bomb[1]):
-                    visited.add(j)
-                    res += dfs(j, visited)
+            for neighbor in graph[i]:
+                if neighbor not in visited:
+                    visited.add(neighbor)
+                    res += dfs(neighbor, visited)
+            
             return res
-
+        
         res = 0
-        for i, bomb in enumerate(bombs):
-            res = max(res, dfs(i, {i}))
 
+        for i in range(len(bombs)):
+            res = max(res, dfs(i, set([i])))
+        
         return res

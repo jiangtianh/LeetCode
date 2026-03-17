83fef421af7a18b63184a5f480c6f7d2c2bd4f1e
Jiangtian Han <112032591+jiangtianh@users.noreply.github.com>
2026-03-17 10:41:50 -0700

    Time: 164 ms (21.51%), Space: 43.4 MB (7.53%) - LeetHub

--- 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.py
+++ 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.py
@@ -1,36 +1,27 @@
 class Solution:
     def largestSubmatrix(self, matrix: List[List[int]]) -> int:
-        li = [[0] * len(matrix[0]) for _ in range(len(matrix))]
-        
-        temp = [0] * len(matrix[0])
+        m, n = len(matrix), len(matrix[0])
 
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
+        consecutiveOnes = [[0] * n for _ in range(m)]
+
+        for j in range(n):
+            count = 0
+            for i in range(m):
                 if matrix[i][j] == 1:
-                    temp[j] += 1
+                    count += 1
                 else:
-                    temp[j] = 0
-
-                li[i][j] = temp[j]
+                    count = 0
+                consecutiveOnes[i][j] = count
 
         res = 0
-        print(li)
-        for i in range(len(li)):
-            li[i].sort(reverse=True)
+        for li in consecutiveOnes:
+            li.sort(reverse=True)   
+            smallest = li[0]
 
-            smallest = li[i][0]
-            for j in range(len(li[0])):
-                if smallest > li[i][j]:
-                    smallest = li[i][j]
-                res = max(res, smallest * (j + 1))
-            
-
-
-
+            for i, num in enumerate(li):
+                if num == 0:
+                    break
+                smallest = min(smallest, num)
+                res = max(res, smallest * (i + 1))
         return res
-
-
-
-
-
-
+                

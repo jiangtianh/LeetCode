eb06fa75b59305d2aac38500431efddc19bf8693
Jiangtian Han <112032591+jiangtianh@users.noreply.github.com>
2026-04-02 11:30:05 -0700

    Time: 43 ms (32.22%), Space: 35.2 MB (15.93%) - LeetHub

--- 0582-kill-process/0582-kill-process.py
+++ 0582-kill-process/0582-kill-process.py
@@ -3,18 +3,15 @@ class Solution:
         graph = collections.defaultdict(list)
 
         for i in range(len(pid)):
-            ID = pid[i]
-            parent = ppid[i]
-            graph[parent].append(ID)
-            if ID not in graph:
-                graph[ID] = []
-        res = []
+            graph[ppid[i]].append(pid[i])
+
+        self.res = []
 
         def f(node):
-            res.append(node)
+            self.res.append(node)
             for child in graph[node]:
                 f(child)
         
         f(kill)
-        return res
-                
+
+        return self.res
